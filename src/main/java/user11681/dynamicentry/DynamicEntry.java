713cee83fa20cb8d1cb4c37282983ae641fca898
user11681 <username11681@gmail.com>
2020-10-18 00:48:22 +0100

    fix incompatibility with loader 0.10.2

--- src/main/java/user11681/dynamicentry/DynamicEntry.java
+++ src/main/java/user11681/dynamicentry/DynamicEntry.java
@@ -3,6 +3,8 @@ package user11681.dynamicentry;
 import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Consumer;
 import net.fabricmc.loader.ModContainer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -21,21 +23,19 @@ public class DynamicEntry {
     /**
      * Load classes specified in the entrypoint <b>{@code name}</b> and execute <b>{@code onLoad}</b> when a class is loaded.
      *
-     * @param name the name of the entrypoint.
+     * @param name   the name of the entrypoint.
      * @param onLoad the callback for when a class is loaded.
      */
     public static void load(final String name, final Consumer<Class<?>> onLoad) {
-        for (final ModContainer mod : FabricLoader.getInstance().getAllMods().toArray(new ModContainer[0])) {
-            for (final EntrypointMetadata entrypoint : mod.getInfo().getEntrypoints(name)) {
-                try {
-                    final Class<?> klass = Class.forName(entrypoint.getValue());
+        for (final EntrypointContainer entrypoint : getEntrypointTargets(name)) {
+            try {
+                final Class<?> klass = Class.forName(entrypoint.entrypoint);
 
-                    if (onLoad != null) {
-                        onLoad.accept(klass);
-                    }
-                } catch (final ClassNotFoundException exception) {
-                    throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.getValue(), name, mod.getMetadata().getName()), exception);
+                if (onLoad != null) {
+                    onLoad.accept(klass);
                 }
+            } catch (final ClassNotFoundException exception) {
+                throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.entrypoint, name, entrypoint.entrypoint), exception);
             }
         }
     }
@@ -43,25 +43,76 @@ public class DynamicEntry {
     /**
      * Execute an entrypoint.
      *
-     * @param name the name of the entrypoint to execute.
+     * @param name           the name of the entrypoint to execute.
      * @param entrypointType the entrypoint type.
-     * @param onExecute entrypoint execution callback with an instance of the entrypoint type.
-     * @param <T> the entrypoint type.
+     * @param onExecute      entrypoint execution callback with an instance of the entrypoint type.
+     * @param <T>            the entrypoint type.
      */
     public static <T> void execute(final String name, final Class<T> entrypointType, final Consumer<T> onExecute) {
+        final ReferenceArrayList<T> entrypointTargets = ReferenceArrayList.wrap((T[]) Array.newInstance(entrypointType, 3), 0);
+        final ReferenceArrayList<EntrypointContainer> entrypoints = getEntrypointTargets(name);
+
+        for (final EntrypointContainer entrypoint : entrypoints) {
+            try {
+                entrypointTargets.add(entrypointType.cast(Class.forName(entrypoint.entrypoint).getConstructor().newInstance()));
+            } catch (final ClassNotFoundException exception) {
+                throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.entrypoint, name, entrypoint.mod), exception);
+            } catch (final IllegalAccessException | InstantiationException | NoSuchMethodException exception) {
+                throw new IllegalStateException(String.format("class %s specified in the %s entrypoint of mod %s cannot be instantiated", entrypoint.entrypoint, name, entrypoint.mod), exception);
+            } catch (final InvocationTargetException exception) {
+                throw new RuntimeException(String.format("an error was encountered during the instantiation of the %s entrypoint class %s", name, entrypoint.entrypoint), exception);
+            }
+        }
+
+        final T[] entrypointArray = entrypointTargets.elements();
+        final int entrypointCount = entrypointTargets.size();
+
+        for (int i = 0; i < entrypointCount; i++) {
+            onExecute.accept(entrypointArray[i]);
+        }
+    }
+
+    /**
+     * Load and execute an entrypoint if its type is implemented.
+     *
+     * @param name           the name of the entrypoint to execute.
+     * @param entrypointType the entrypoint type.
+     * @param onExecute      entrypoint execution callback with an instance of the entrypoint type.
+     * @param <T>            the entrypoint type.
+     */
+    public static <T> void tryExecute(final String name, final Class<T> entrypointType, final Consumer<T> onExecute) {
+        tryExecute(name, entrypointType, onExecute, null);
+    }
+
+    /**
+     * Load and execute an entrypoint if its type is implemented.
+     *
+     * @param name           the name of the entrypoint to execute.
+     * @param entrypointType the entrypoint type.
+     * @param onExecute      entrypoint execution callback with an instance of the entrypoint type.
+     * @param onLoad         entrypoint class load callback.
+     * @param <T>            the entrypoint type.
+     */
+    public static <T> void tryExecute(final String name, final Class<T> entrypointType, final Consumer<T> onExecute, final Consumer<Class<?>> onLoad) {
         final ReferenceArrayList<T> entrypoints = ReferenceArrayList.wrap((T[]) Array.newInstance(entrypointType, 3), 0);
 
-        for (final ModContainer mod : FabricLoader.getInstance().getAllMods().toArray(new ModContainer[0])) {
-            for (final EntrypointMetadata entrypoint : mod.getInfo().getEntrypoints(name)) {
-                try {
-                    entrypoints.add(entrypointType.cast(Class.forName(entrypoint.getValue()).getConstructor().newInstance()));
-                } catch (final ClassNotFoundException exception) {
-                    throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.getValue(), name, mod.getMetadata().getName()), exception);
-                } catch (final IllegalAccessException | InstantiationException | NoSuchMethodException exception) {
-                    throw new IllegalStateException(String.format("class %s specified in the %s entrypoint of mod %s cannot be instantiated", entrypoint.getValue(), name, mod.getMetadata().getName()), exception);
-                } catch (final InvocationTargetException exception) {
-                    throw new RuntimeException(String.format("an error was encountered during the instantiation of the %s entrypoint class %s", name, entrypoint.getValue()), exception);
+        for (final EntrypointContainer entrypoint : getEntrypointTargets(name)) {
+            try {
+                final Class<?> klass = Class.forName(entrypoint.entrypoint);
+
+                if (onLoad != null) {
+                    onLoad.accept(klass);
                 }
+
+                if (entrypointType.isAssignableFrom(klass)) {
+                    entrypoints.add(entrypointType.cast(klass.getConstructor().newInstance()));
+                }
+            } catch (final ClassNotFoundException exception) {
+                throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.entrypoint, name, entrypoint.mod), exception);
+            } catch (final IllegalAccessException | InstantiationException | NoSuchMethodException exception) {
+                throw new IllegalStateException(String.format("class %s specified in the %s entrypoint of mod %s cannot be instantiated", entrypoint.entrypoint, name, entrypoint.mod), exception);
+            } catch (final InvocationTargetException exception) {
+                throw new RuntimeException(String.format("an error was encountered during the instantiation of the %s entrypoint class %s", name, entrypoint.entrypoint));
             }
         }
 
@@ -73,57 +124,19 @@ public class DynamicEntry {
         }
     }
 
-    /**
-     * Load and execute an entrypoint if its type is implemented.
-     *
-     * @param name the name of the entrypoint to execute.
-     * @param entrypointType the entrypoint type.
-     * @param onExecute entrypoint execution callback with an instance of the entrypoint type.
-     * @param <T> the entrypoint type.
-     */
-    public static <T> void maybeExecute(final String name, final Class<T> entrypointType, final Consumer<T> onExecute) {
-        maybeExecute(name, entrypointType, onExecute, null);
-    }
+    private static ReferenceArrayList<EntrypointContainer> getEntrypointTargets(final String entrypoint) {
+        final ReferenceArrayList<EntrypointContainer> entrypoints = ReferenceArrayList.wrap(new EntrypointContainer[1], 0);
 
-    /**
-     * Load and execute an entrypoint if its type is implemented.
-     *
-     * @param name the name of the entrypoint to execute.
-     * @param entrypointType the entrypoint type.
-     * @param onExecute entrypoint execution callback with an instance of the entrypoint type.
-     * @param onLoad entrypoint class load callback.
-     * @param <T> the entrypoint type.
-     */
-    public static <T> void maybeExecute(final String name, final Class<T> entrypointType, final Consumer<T> onExecute, final Consumer<Class<?>> onLoad) {
-        final ReferenceArrayList<T> entrypoints = ReferenceArrayList.wrap((T[]) Array.newInstance(entrypointType, 3), 0);
+        for (final ModContainer mod : (Collection<ModContainer>) (Object) FabricLoader.getInstance().getAllMods()) {
+            final List<EntrypointMetadata> modEntrypoints = mod.getInfo().getEntrypoints(entrypoint);
 
-        for (final ModContainer mod : FabricLoader.getInstance().getAllMods().toArray(new ModContainer[0])) {
-            for (final EntrypointMetadata entrypoint : mod.getInfo().getEntrypoints(name)) {
-                try {
-                    final Class<?> klass = Class.forName(entrypoint.getValue());
-
-                    if (onLoad != null) {
-                        onLoad.accept(klass);
-                    }
-
-                    if (entrypointType.isAssignableFrom(klass)) {
-                        entrypoints.add(entrypointType.cast(klass.getConstructor().newInstance()));
-                    }
-                } catch (final ClassNotFoundException exception) {
-                    throw new IllegalArgumentException(String.format("class %s specified in the %s entrypoint of mod %s does not exist", entrypoint.getValue(), name, mod.getMetadata().getName()), exception);
-                } catch (final IllegalAccessException | InstantiationException | NoSuchMethodException exception) {
-                    throw new IllegalStateException(String.format("class %s specified in the %s entrypoint of mod %s cannot be instantiated", entrypoint.getValue(), name, mod.getMetadata().getName()), exception);
-                } catch (final InvocationTargetException exception) {
-                    throw new RuntimeException(String.format("an error was encountered during the instantiation of the %s entrypoint class %s", name, entrypoint.getValue()));
+            if (modEntrypoints != null) {
+                for (final EntrypointMetadata metadata : modEntrypoints) {
+                    entrypoints.add(new EntrypointContainer(metadata.getValue(), mod.getMetadata().getId()));
                 }
             }
         }
 
-        final T[] entrypointArray = entrypoints.elements();
-        final int entrypointCount = entrypoints.size();
-
-        for (int i = 0; i < entrypointCount; i++) {
-            onExecute.accept(entrypointArray[i]);
-        }
+        return entrypoints;
     }
 }
